126dc02efb329a1a4c148b4eeaded50d5bd720b6
kushal-das11 <kushal.das2@cognizant.com>
2026-05-31 22:30:55 +0530

    Invemtory test added

--- services/inventory-service/src/test/java/com/cts/inventory/InventoryTest.java
+++ services/inventory-service/src/test/java/com/cts/inventory/InventoryTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class InventoryServiceTest {
+class InventoryTest {
 
     @Mock
     private InventoryRepository repository;
